d436569beeadbdf2a3022e7cbba696813bf5c479
Xinwei Guo <xguo50@binghamton.edu>
2025-05-11 15:07:00 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const section = document.getElementById(targetId);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+
+const toggleBtn = document.getElementById('toggle-theme');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('light-mode');
+  if (document.body.classList.contains('light-mode')) {
+    toggleBtn.textContent = '☀️ Light Mode';
+  } else {
+    toggleBtn.textContent = '🌙 Dark Mode';
+  }
+});
+
+  
